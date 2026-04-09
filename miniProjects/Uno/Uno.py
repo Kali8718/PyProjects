faed324903496d4d47b1f7edd228b127c9c98d74
Kali8718 <alkharassani87@gmail.com>
2026-04-09 18:04:39 +0100

    xd

--- miniProjects/Uno/Uno.py
+++ miniProjects/Uno/Uno.py
@@ -1,6 +1,33 @@
 import random
 
-set_list = []
+#Class players to emulate players, each player is an instance
+
+class Players :
+    position = 1
+    
+    def __init__(self, name):
+        Players.position += 1
+        
+        self.name = name
+        self.position = Players.position
+        self.cards = []       
+        
+    def normal_pull(self, set) :
+        self.cards.append(set.pop())     
+
+    def intitial_pull(self, set) :
+        for i in range(0,4) :
+            self.normal_pull(set)
+
+            
+            
+
+
+
+#set of functions to create and shuffle the set
+
+draw_pile = []
+discard_pile = []
 
 colors = ['G','B','Y','R']
 numbers = []
@@ -13,13 +40,11 @@ for i in range (0,11) :
         numbers.append(i)
 
 
-
 def generate_set(set) :
     for number in numbers :
         for color in colors :
             added_card = f'{number}{color}'
             set.append(added_card)
-            
             
 
 def shuffle_set(set) :
@@ -36,10 +61,18 @@ def ask_number_players() :
     return num_players
 
 
-  
 
-generate_set(set_list)
-shuffle_set(set_list)
-ask_number_players()
-print(set_list)
 
+#Section to use the functions 
+
+player_1 = Players('Ali')
+player_2 = Players('Othmane')
+
+generate_set(draw_pile)
+shuffle_set(draw_pile)
+
+print(player_1.cards)
+
+player_1.intitial_pull(draw_pile)
+
+print(player_1.cards)
