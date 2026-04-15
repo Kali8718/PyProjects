53c97f15589facbc4740b7379df211b5b9cde1cd
Kali8718 <alkharassani87@gmail.com>
2026-04-15 11:31:18 +0100

    Update Uno.py

--- miniProjects/Uno/Uno.py
+++ miniProjects/Uno/Uno.py
@@ -7,36 +7,46 @@ import random
 
 class Players :
     position = 1
+    all_players = []
     
     def __init__(self, name):
         Players.position += 1
-        
         self.name = name
         self.position = Players.position
         self.cards = []       
+        Players.all_players.append(self)
         
-    def normal_pull(self, set) :
+    def draw_card(self, set) :
         self.cards.append(set.pop(0))     
 
-    def intitial_pull(self, set) :
+    def initial_draw(self, set) :
         for i in range(0,4) :
-            self.normal_pull(set)
+            self.draw_card(set)
+            
+    def check_card_in_hand(self, card):
+        if card.upper() in self.cards :
+            return True
+        return False
         
-    
     def play_card(self) :
-        #first we check if the card is in the player's hand (can be re-written as a function later and called)
         while True:
-            played_card = input("Select a card to play: ")
-            if played_card.upper() in self.cards :
-                print(f"{self.name} has played {played_card}")
-                break
-            else :
-                print(f"card not in hand, re-enter it {[self.cards]}")
-        #Now we check if the played card is correct to play
-        for char in played_card :
-            pass
+            #first we check if the card is in the player's hand (can be re-written as a function later and called)
+            while True:
+                played_card = input("Select a card to play: ")
+                if played_card.upper() in self.cards :
+                    print(f"{self.name} has played {played_card}")
+                    break
+                else :
+                    print(f"card not in hand, re-enter it {[self.cards]}")
+            #Now we check if the played card is correct to play
+            for char in played_card:
+                if char in discard_pile[-1] :
+                    discard_pile.append(played_card)
+                    return
+            print("Card can not be played, please Select an appropriate card or Draw one")
             
 
+            
 
 
 
@@ -82,7 +92,7 @@ def ask_number_players() :
 
 
 
-#Section to use the functions 
+#fuck around and find out section
 
 player_1 = Players('Ali')
 player_2 = Players('Akram')
@@ -90,12 +100,15 @@ player_2 = Players('Akram')
 generate_set(draw_pile)
 shuffle_set(draw_pile)
 
-print(player_1.cards)
 
-player_1.intitial_pull(draw_pile)
 
-print(player_1.cards)
+player_1.initial_draw(draw_pile)
 
 print(discard_pile)
 draw_initial_discard_pile(draw_pile)
 print(f"Last played card: {draw_pile[-1]}")
+
+
+while True :
+    for player in Players.all_players:
+        print(f"{player.cards}")
